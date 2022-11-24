6d11d5aeae9dc742da608bd6eea703223c8126f1
95wjddbs <66461799+95wjddbs@users.noreply.github.com>
2022-11-24 15:25:10 +0900

    tweet create and read 1124

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,27 +1,68 @@
+import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
 
+//인증 page
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState(""); // 이메일 입력
+  const [password, setPassword] = useState(""); // 비밀번호 입력
+  const [newAccount, setNewAccount] = useState(true); // 새 계정 생성
+  const [error, setError] = useState(""); // 에러 체크
+
+  //이메일, 비밀번호 구분하고, 변화를 위한 함수
   const onChange = (event) => {
     const {
       target: { name, value },
     } = event;
+    console.log(value);
     if (name === "email") {
       setEmail(value);
     } else if (name === "password") {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+
+  // 로그인 제출 버튼을 위한 함수
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  //새 계정인지 아닌지 판단을 위한 함수
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  //소셜 로그인 (깃허브, 구글) 판단을 위한 함수
+  const onSocialClick = (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = authService.signInWithPopup(provider);
+    console.log(data);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
           name="email"
-          type="text"
+          type="email"
           placeholder="Email"
           required
           value={email}
@@ -35,11 +76,22 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type={"submit"} value="Log In" />
+        <input
+          type={"submit"}
+          value={newAccount ? "Create Account" : "Log In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
+import { serverTimestamp } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createAt: serverTimestamp(),
+    });
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  console.log(nweets);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          type={"text"}
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type={"submit"} value={"Nweet"} />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>{" "}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
